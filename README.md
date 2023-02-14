3551b83b0e858cb7f53978ff7939ff398bd48b9f
valeriia-nurieva <valerka.0906@gmail.com>
2023-02-14 13:06:59 +0200

    create CLI app

---
+++ README.md
@@ -0,0 +1,23 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+
+https://monosnap.com/file/pmGSH4n3wQyUaOgtmc1mC8EGroBpRN
+
+# Отримуємо контакт по id
+
+node index.js --action="get" --id=5
+
+https://monosnap.com/file/GxMB1r318PVfOuPbmwpIiEj8YCMSXG
+
+# Додаємо контакт
+
+node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22"
+
+https://monosnap.com/file/wSATZAW4e0mA2mnjZG1T8VHEaJx6f0
+
+# Видаляємо контакт
+
+node index.js --action="remove" --id=3
+
+https://monosnap.com/file/P4YbBx0uJ3mXKDLzsw1Ku9ptvV2Cy0
---
+++ contacts.js
@@ -0,0 +1,55 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find(({ id }) => id === contactId);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const changedContacts = contacts.filter(({ id }) => id !== contactId);
+    fs.writeFile(contactsPath, JSON.stringify(changedContacts));
+    return contacts.filter(({ id }) => id === contactId);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: uuidv4(), name: name, email: email, phone: phone };
+    const contacts = await listContacts();
+    const changedContacts = [...contacts, newContact];
+    fs.writeFile(contactsPath, JSON.stringify(changedContacts));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
