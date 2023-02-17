3eca4c4ff1649102c4ab51c83d8ff83992190886
valeriia-nurieva <valerka.0906@gmail.com>
2023-02-17 14:56:15 +0200

    fix json.stringify

--- contacts.js
+++ contacts.js
@@ -18,7 +18,7 @@ async function getContactById(contactId) {
   try {
     const contacts = await listContacts();
     const result = contacts.find(({ id }) => id === contactId);
-    return result;
+    return result || null;
   } catch (error) {
     console.log(error.message);
   }
@@ -28,7 +28,7 @@ async function removeContact(contactId) {
   try {
     const contacts = await listContacts();
     const changedContacts = contacts.filter(({ id }) => id !== contactId);
-    fs.writeFile(contactsPath, JSON.stringify(changedContacts));
+    fs.writeFile(contactsPath, JSON.stringify(changedContacts, null, 2));
     return contacts.filter(({ id }) => id === contactId);
   } catch (error) {
     console.log(error.message);
@@ -37,10 +37,10 @@ async function removeContact(contactId) {
 
 async function addContact(name, email, phone) {
   try {
-    const newContact = { id: uuidv4(), name: name, email: email, phone: phone };
+    const newContact = { id: uuidv4(), name, email, phone };
     const contacts = await listContacts();
     const changedContacts = [...contacts, newContact];
-    fs.writeFile(contactsPath, JSON.stringify(changedContacts));
+    fs.writeFile(contactsPath, JSON.stringify(changedContacts, null, 2));
     return newContact;
   } catch (error) {
     console.log(error.message);
